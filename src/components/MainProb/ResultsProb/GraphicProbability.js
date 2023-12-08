f8f3ebb22c006dc60a193edaa3e10a70263808cd
Sukhov Egor <sukhov123net@yandex.ru>
2023-12-08 14:50:47 +0700

    график

--- src/components/MainProb/ResultsProb/GraphicProbability.js
+++ src/components/MainProb/ResultsProb/GraphicProbability.js
@@ -1,14 +1,27 @@
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend} from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, CartesianGrid} from 'recharts';
 
 const GraphicInit = (props) => {
+    let resultArray = props.calculationResults?.calculationResults;
+    const data = []
+    if (resultArray.length > 1) {
+        for (let i = 0; i < resultArray.length; i++) {
+
+            data.push(
+                {
+                    time: resultArray[i].urovValue,
+                    probability: resultArray[i].probabilityValue * 100
+                })
+        }
+    }
     return <div >
         <div className="chart">
-            <LineChart width={560} height={300} data={props.calculationResults} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                <XAxis dataKey="urovValue" />
-                <YAxis unit={props.measure} />
+            <LineChart width={560} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="time" />
+                <YAxis unit='%' />
                 <Tooltip />
                 <Legend />
-                <Line type="momotone" dataKey="probabilityValue" name={props.name} stroke="#8884d8" dot={false} strokeWidth={3} />
+                <Line type="momotone" dataKey="probability" name='Вероятность излишней работы УРОВ' stroke="#8884d8" dot={false} strokeWidth={3} />
             </LineChart>
         </div>
     </div>;
--- src/components/MainProb/ResultsProb/ResultTable.js
+++ src/components/MainProb/ResultsProb/ResultTable.js
@@ -2,41 +2,40 @@ import { Table } from "antd";
 import { useParams } from 'react-router-dom';
 
 const ResultTable = (props) => {
-const params = useParams();
+    const params = useParams();
 
-let index = props.calculationResults?.calculationResults;
-console.log(index.length)
+    let resultArray = props.calculationResults?.calculationResults;
 
-const columns = [
-    {
-        title: 'Выдежка времени УРОВ',
-        dataIndex: 'time',
-        key: 'time',
-    },
-    {
-        title: 'Вероятность излишней работы УРОВ',
-        dataIndex: 'probability',
-        key: 'probability',
+
+    const columns = [
+        {
+            title: 'Выдежка времени УРОВ',
+            dataIndex: 'time',
+            key: 'time',
+        },
+        {
+            title: 'Вероятность излишней работы УРОВ',
+            dataIndex: 'probability',
+            key: 'probability',
+        }
+    ]
+    const data = []
+    if (resultArray.length > 1) {
+        for (let i = 0; i < resultArray.length; i++) {
+
+            data.push(
+                {
+                    key: resultArray[i].implementationId,
+                    time: resultArray[i].urovValue,
+                    probability: resultArray[i].probabilityValue
+                })
+        }
     }
-]
-const data = []
-if(index.length > 1){
-    for (let i = 0; i<index.length; i++){
-        
-        data.push(
-            {
-                key: index[i].implementationId,
-                time:  index[i].urovValue,
-                probability: index[i].probabilityValue
-        
-            })
-    }
-}
 
 
-return (
-    <Table columns={columns} dataSource={data} />
-)
+    return (
+        <Table columns={columns} dataSource={data} pagination={{pageSize: 6}}/>
+    )
 }
 
 export default ResultTable;
